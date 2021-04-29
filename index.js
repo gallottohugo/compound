25eb719877775200af9fe076ee4059685a462d36
gallottohugo <gallottohugo@gmail.com>
2021-04-29 19:47:40 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const Web3 = require('web3')
+const web3 = new Web3("https://ropsten.infura.io/v3/19beaa22507c40a4b0986bc5eebcf14c")
+
+//Metamask ropsten address
+const address1 = "0xA530c8A7c550486Ca6B96219D775A61e0c3F5BdA";
+const address1Key = new Buffer.from('848f7e3e0ffbce3c0ed878027d9700b4d1fc3fa099ba8bc6a5667fc5aff13519', 'hex')
+
